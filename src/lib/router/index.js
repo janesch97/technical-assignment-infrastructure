12f0ac69e699809ec52fe6fa3a6f666f3647f5b5
Tomislav Janeš <tomis.janes@gmail.com>
2021-05-15 15:10:41 +0200

    add stubs for WebApp and Router classes

--- src/lib/router/index.js
+++ src/lib/router/index.js
@@ -3,3 +3,18 @@
  *
  * This module should export your implementation of Router class.
  */
+class Router {
+  get() {
+    // TODO: Implement get(route, handler)
+  }
+
+  post() {
+    // TODO: Implement post(route, handler)
+  }
+
+  routes() {
+    // TODO: Implement routes(options)
+  }
+}
+
+module.exports = Router;
